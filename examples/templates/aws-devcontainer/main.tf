e5f7250015492eac82b2f7cda0aafaee9cf77264
Wanjohi <156436993+ryanwanjohi@users.noreply.github.com>
2024-01-15 09:28:14 +0300

    Update main.tf

--- examples/templates/aws-devcontainer/main.tf
+++ examples/templates/aws-devcontainer/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 module "aws_region" {
   source  = "https://registry.coder.com/modules/aws-region"
-  default = "us-east-1"
+  default = "af-south-1"
 }
 
 data "coder_parameter" "instance_type" {
@@ -48,6 +48,25 @@ data "coder_parameter" "instance_type" {
 
 provider "aws" {
   region = module.aws_region.value
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "coder-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["af-south-1a", "af-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 data "coder_workspace" "me" {
